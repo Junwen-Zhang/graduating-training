5766869df53ad30a750fa8b6f44306f0a0b8b3e2
JuneWen <june_zjw@qq.com>
2022-06-23 08:38:32 +0800

    zjw鬼畜差词云图

--- code/douban_flask/static/assets/main/show_guichu_length.js
+++ code/douban_flask/static/assets/main/show_guichu_length.js
@@ -1,46 +1,56 @@
 (function (func) {
-    $.ajax({
-        url: "/guichu/lengthAnalyse",
-        type: "GET",
-        dataType: "json",
-        success: function (data) {
-            func(data);
-        }
-    });
+  $.ajax({
+    url: "/guichu/lengthAnalyse",
+    type: "GET",
+    dataType: "json",
+    success: function (data) {
+      func(data);
+    }
+  });
 })(function (data) {
-    var myChart = echarts.init(document.getElementById('chart_guichu_length'), 'infographic');
+  var myChart = echarts.init(document.getElementById('chart_guichu_length'), 'infographic');
 
-    var option = {
-  legend: {
-    top: 'bottom'
-  },
-  toolbox: {
-    show: true,
-    feature: {
-      mark: { show: true },
-      dataView: { show: true, readOnly: false },
-      restore: { show: true },
-      saveAsImage: { show: true }
-    }
-  },
-  series: [
-    {
-      name: 'Nightingale Chart',
-      type: 'pie',
-      radius: [50, 250],
-      center: ['50%', '50%'],
-      roseType: 'area',
-      itemStyle: {
-        borderRadius: 8
-      },
-      data: data
-    }
-  ]
-};
+  var option = {
+    tooltip: {
+      trigger: 'item'
+    },
+    legend: {
+      top: '5%',
+      left: 'center'
+    },
+    series: [
+      {
+        name: 'Access From',
+        type: 'pie',
+        radius: ['40%', '70%'],
+        avoidLabelOverlap: false,
+        itemStyle: {
+          borderRadius: 10,
+          borderColor: '#fff',
+          borderWidth: 2
+        },
+        label: {
+          show: false,
+          position: 'center'
+        },
+        emphasis: {
+          label: {
+            show: true,
+            fontSize: '40',
+            fontWeight: 'bold'
+          }
+        },
+        labelLine: {
+          show: false
+        },
+        data: data
+      }
+    ]
+  };
 
-    myChart.setOption(option);
-    window.addEventListener("resize", function () {
-        myChart.resize();
-    });
+  myChart.setOption(option);
+  window.addEventListener("resize", function () {
+    myChart.resize();
+  });
 
 });
