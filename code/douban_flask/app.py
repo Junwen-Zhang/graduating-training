93fe641f446c02074b6ea84b1cb00bf30160ce45
JuneWen <june_zjw@qq.com>
2022-06-23 17:22:31 +0800

    B-2 文字部分完成

--- code/douban_flask/app.py
+++ code/douban_flask/app.py
@@ -5,14 +5,14 @@
 from config import app
 
 from views.index_view import index
-from views.animation_view import animation
+# from views.animation_view import animation
 from views.guichu_view import guichu
-from views.up_view import up
+# from views.up_view import up
 
-app.register_blueprint(animation, url_prefix="/animation")
+# app.register_blueprint(animation, url_prefix="/animation")
 app.register_blueprint(guichu, url_prefix="/guichu")
 app.register_blueprint(index, url_prefix="/")
-app.register_blueprint(up, url_prefix="/up")
+# app.register_blueprint(up, url_prefix="/up")
 
 if __name__ == '__main__':
     app.run(debug=True)
