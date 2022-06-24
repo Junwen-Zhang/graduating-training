7427181f6f33c5df5d4262c88a424d30e1bc540b
JuneWen <june_zjw@qq.com>
2022-06-24 15:04:58 +0800

    guichu dbmodel

--- code/douban_flask/views/video_view.py
+++ code/douban_flask/views/video_view.py
@@ -4,7 +4,7 @@ import json
 from flask import Blueprint, jsonify, request, render_template
 
 from config import db
-from dbmodel.video import VideoLength,VideoSubmitHour,VideoTags
+from dbmodel.video import Video,VideoKeyword,VideoTime
 
 
 """
@@ -16,9 +16,10 @@ video = Blueprint('video', __name__)
 def videoAnalysis():
     return render_template("video_main.html")
 
-@video.route('/show100video')
+
+@video.route('/showhot/<id>')
 def show100video():
-    data = db.session.query(video).all()
+    data = db.session.query(video(id)).all()
     view_data = {}
     view_data["series"] = []
     def build_view_data(item):
@@ -28,7 +29,7 @@ def show100video():
         dic["picture_url"] = item.picture_url
         dic["followers_number"] = item.followers_number
         dic["episodes"] = item.episodes
-        dic["time"] = item.time
+        dic["score"] = item.score
         dic["label"]=item.label
         dic["introduction"]=item.introduction
         view_data["series"].append([dic])
@@ -37,11 +38,12 @@ def show100video():
         build_view_data(item)
     return render_template("show100video.html",fanjus=view_data)
 
+
 @video.route('/time')
 def videotime():
     return render_template("show_video_time.html")
 
-@video.route('/timeAnalyse',methods=['GET'])
+@video.route('/timeAnalyse/<id>',methods=['GET'])
 def videotimeAnalyse():
     data = db.session.query(videotime).all()
     view_data = {}
@@ -57,7 +59,7 @@ def videotimeAnalyse():
 def videokeyword():
     return render_template("show_video_keyword.html")
 
-@video.route('/keywordAnalyse',methods=['GET'])
+@video.route('/keywordAnalyse/<id>',methods=['GET'])
 def videokeywordAnalyse():
     data = db.session.query(videokeyword).all()
     view_data = {}
@@ -70,35 +72,3 @@ def videokeywordAnalyse():
     [build_view_data(item) for item in data]
     return json.dumps(view_data, ensure_ascii=False)  # 将python对象转化为json对象
 
-
-@video.route('/tagsAnalyse',methods=['GET'])
-def videoTagsAnalyse():
-    Tags_dict = getTagsDict()
-    view_data={}
-    view_data["series1"] = []
-    def build_view_data(Tags_dict):
-        for key,value in Tags_dict.items():
-            data = {}
-            data['name'] = key
-            data['value'] = value
-            view_data['series1'].append(data)
-    build_view_data(Tags_dict)
-
-    Tags_dict2 = get100TagsDict()
-    view_data["series2"] = []
-    def build_view_data2(Tags_dict):
-        for key, value in Tags_dict.items():
-            data = {}
-
-            data['name'] = key
-            data['value'] = value
-            view_data['series2'].append(data)
-    build_view_data2(Tags_dict2)
-    view_data['maskImage']='./img/tree.jpg'
-    # print(view_data)
-    return json.dumps(view_data, ensure_ascii=False)  # 将python对象转化为json对象
-
-
-@video.route('/animeTags')
-def videoTags():
-    return render_template("video_tags.html")
