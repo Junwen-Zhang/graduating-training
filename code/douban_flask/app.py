17a440bbb4764eb9377ed992905555ddcdd6381c
zhang-qian907 <1779566457@qq.com>
2022-06-27 08:51:16 +0800

    框架

--- code/douban_flask/app.py
+++ code/douban_flask/app.py
@@ -9,13 +9,13 @@ from views.index_view import index
 from views.guichu_view import guichu
 # from views.up_view import up
 from views.video_view import video
-
+from views.searchVideo_view import search
 # app.register_blueprint(animation, url_prefix="/animation")
 app.register_blueprint(guichu, url_prefix="/guichu")
 app.register_blueprint(index, url_prefix="/")
 # app.register_blueprint(up, url_prefix="/up")
 app.register_blueprint(video, url_prefix="/video")
-
+app.register_blueprint(search,url_prefix="/search")
 
 
 if __name__ == '__main__':
