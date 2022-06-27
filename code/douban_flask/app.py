bda7a34033a20ca5d35da3ef1124fb9affda1552
lqq-lqq <1602020796@qq.com>
2022-06-27 10:58:40 +0800

    番剧分析雏形

--- code/douban_flask/app.py
+++ code/douban_flask/app.py
@@ -5,33 +5,20 @@
 from config import app
 
 from views.index_view import index
-<<<<<<< HEAD
 
 
 
 from views.animation_view import animation
 # from views.guichu_view import guichu
-from views.up_view import up
-# from views.video_view import video
-
+# from views.up_view import up
+from views.video_view import video
+# from views.searchVideo_view import search
 app.register_blueprint(animation, url_prefix="/animation")
 # app.register_blueprint(guichu, url_prefix="/guichu")
 app.register_blueprint(index, url_prefix="/")
-app.register_blueprint(up, url_prefix="/up")
-# app.register_blueprint(video, url_prefix="/video")
-=======
-# from views.animation_view import animation
-from views.guichu_view import guichu
-# from views.up_view import up
-from views.video_view import video
-from views.searchVideo_view import search
-# app.register_blueprint(animation, url_prefix="/animation")
-app.register_blueprint(guichu, url_prefix="/guichu")
-app.register_blueprint(index, url_prefix="/")
 # app.register_blueprint(up, url_prefix="/up")
-app.register_blueprint(video, url_prefix="/video")
-app.register_blueprint(search,url_prefix="/search")
->>>>>>> 17a440bbb4764eb9377ed992905555ddcdd6381c
+# app.register_blueprint(video, url_prefix="/video")
+# app.register_blueprint(search,url_prefix="/search")
 
 
 if __name__ == '__main__':
